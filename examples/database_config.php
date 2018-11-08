9661c00406981fbfa560639b8f1fb7280f799a57
michelleoraaali <ali26m@mtholyoke.edu>
2018-11-08 01:08:14 -0500

    modifying php
    Built on JsPsych (modified some plug-ins so not vanilla JsPsych). Work with Paula Rubio-Fernandez and Julian Jara-Ettinger.

--- examples/database_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $servername = "http://michoa.scripts.mit.edu/RefComm_vis_pilot/examples/";
-  $port = 3306;
-  $username = "michoa";
-  $password = "wak29dil";
-  $dbname = "matcol";
-  $table = "tablename";
-?>
